fe59dbae9fc59dd17a546130679f7ff57e5c2837
Chris Chou <chhschou@gmail.com>
2018-08-26 21:45:07 +1200

    add scoped style

--- datepicker.js
+++ datepicker.js
@@ -2,7 +2,16 @@ import { html, render } from "https://unpkg.com/lit-html?module";
 
 const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 const now = new Date();
+const style = html`
+<style>
+  .container {
+    display: flex;
+    background: red;
+  }
+</style>
+`;
 const template = html`
+${style}
 <div class="container">
   ${days.map(d => html`<div>${d}</div>`)}
 </div>
