07668077231c1ee66b052b365f08a40169333359
Chris Chou <chhschou@gmail.com>
2018-08-26 21:34:36 +1200

    can repeat/map template

--- datepicker.js
+++ datepicker.js
@@ -1,11 +1,18 @@
 import { html, render } from "https://unpkg.com/lit-html?module";
 
+const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const now = new Date();
+const template = html`
+<div class="container">
+  ${days.map(d => html`<div>${d}</div>`)}
+</div>
+`;
+
 class DatePicker extends HTMLElement {
   constructor() {
     super();
 
     const shadowRoot = this.attachShadow({ mode: "open" });
-    const template = html`<h1>hello world</h1>`;
     render(template, shadowRoot);
   }
 }
